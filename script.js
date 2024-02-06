78f3d4b9ab2cbaa286f1c37363d96d49acac5782
anastasiigonemad <anastasiiakucheryavaya@gmail.com>
2024-02-06 22:34:39 +0300

    add new html files

--- script.js
+++ script.js
@@ -107,6 +107,68 @@ document.querySelector('.burger__menu').addEventListener('click', function() {
   document.querySelector('.burger__nav').classList.toggle('open');
 })
 
+function displayBigImg () {
+  const img = document.querySelector('.diplom-img');
+  const zoom = document.createElement('div');
+
+  zoom.className = 'zoom';
+  zoom.style.position = 'absolute';
+  zoom.style.top = '0';
+  zoom.style.left = '0';
+  zoom.style.width = '100%';
+  zoom.style.height = '100%';
+  zoom.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  zoom.style.display = 'none';
+  img.parentNode.insertBefore(zoom, img);
+
+  const imgClone = img.cloneNode();
+  imgClone.style.position = 'absolute';
+  imgClone.style.top = '50%';
+  imgClone.style.left = '50%';
+  imgClone.style.width = '100%';
+  imgClone.style.height = '100%';
+  imgClone.style.marginTop = '-50%';
+  imgClone.style.marginLeft = '-50%';
+  imgClone.style.opacity = '0.5';
+  imgClone.style.zIndex = '1';
+  zoom.append(imgClone);
+
+  const imgOverlay = document.createElement('div');
+  imgOverlay.className = 'img-overlay';
+  imgOverlay.style.position = 'absolute';
+  imgOverlay.style.top = '0';
+  imgOverlay.style.left = '0';
+  imgOverlay.style.width = '100%';
+  imgOverlay.style.height = '100%';
+  imgOverlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  imgOverlay.style.zIndex = '2';
+  zoom.append(imgOverlay);
+
+  const imgOverlayText = document.createElement('div');
+  imgOverlayText.className = 'img-overlay-text';
+  imgOverlayText.style.position = 'absolute';
+  imgOverlayText.style.top = '50%';
+  imgOverlayText.style.left = '50%';
+  imgOverlayText.style.width = '100%';
+  imgOverlayText.style.height = '100%';
+  imgOverlayText.style.marginTop = '-50%';
+  imgOverlayText.style.marginLeft = '-50%';
+  imgOverlayText.style.color = 'white';
+  imgOverlayText.style.zIndex = '3';
+  imgOverlay.append(imgOverlayText);
+
+  const imgOverlayTextContent = document.createTextNode('Нажмите, чтобы закрыть');
+  imgOverlayText.append(imgOverlayTextContent);
+
+  zoom.addEventListener('click', function() {
+    zoom.style.display = 'none';
+  });
+  
+  img.style.display = 'none';
+  zoom.style.display = 'block';
+};
+
+document.querySelector('.diplom-img').addEventListener('click', displayBigImg);
 
 
 
