6c3f110aa645eca93d228458b63ff1cf6ff40eaf
anastasiigonemad <anastasiiakucheryavaya@gmail.com>
2024-02-12 00:26:54 +0300

    fix all owl-carousel

--- owl/owl.js
+++ owl/owl.js
@@ -1,18 +1,28 @@
 $(document).ready(function(){
       $('#owl-1').owlCarousel({
-        items: 1
+        items: 1,
+        loop: true
       });
     });
 
 $(document).ready(function(){
         $('#owl-2').owlCarousel({
-            items: 1
+            items: 1,
+            loop: true
         });
     });
 
 $(document).ready(function(){
         $('#owl-3').owlCarousel({
-            items: 1
+            items: 1,
+            loop: true
+        });
+    });
+
+$(document).ready(function(){
+        $('#owl-4').owlCarousel({
+            items: 1,
+            loop: true
         });
     });
 
